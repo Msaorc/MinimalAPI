c60bb31def088b80f34597a8ff8cabaee7878881
Marcos Augusto <msaorc@hotmail.com>
2023-01-23 17:21:12 -0300

    Learning about Authentication and Authorization with JWT Bearer

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using System.Security.Claims;
 using apiwithjwt;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using apiwithjwt.Models;
+using apiwithjwt.Repositories;
+using apiwithjwt.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var key = Encoding.ASCII.GetBytes(Settings.Secret);
@@ -25,10 +29,55 @@ builder.Services.AddAuthorization(options =>{
     options.AddPolicy("Employee", policy => policy.RequireRole("employee"));
 });
 
+
 var app = builder.Build();
 
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/login", (User model) =>
+{
+    var user = UserRepository.Get(model.UserName, model.PassWord);
+
+    if (user == null)
+    {
+        return Results.NotFound(new {message = "Invalid username or password"});
+    }
+
+    var token = TokenService.GenerateToken(user);
+
+    user.PassWord = "";
+
+    return Results.Ok(new
+    {
+        user = user,
+        token = token
+    });
+});
+
+app.MapGet("/anonymous", () =>
+{
+    Results.Ok("anonymousMethod");
+}).AllowAnonymous();
+
+app.MapGet("/authenticated", (ClaimsPrincipal user) =>
+{
+    Results.Ok(new { message = $"Autenticated as {user.Identity.Name}"});
+}).RequireAuthorization();
+
+app.MapGet("/employee", (ClaimsPrincipal user) =>
+{
+    Results.Ok(new
+    {
+        message = $"Autenticated as {user.Identity.Name}"
+    });
+}).RequireAuthorization("Employee");
+
+app.MapGet("/manager", (ClaimsPrincipal user) =>
+{
+    Results.Ok(new
+    {
+        message = $"Autenticated as {user.Identity.Name}"
+    });
+}).RequireAuthorization("Admin");
 
 app.Run();
